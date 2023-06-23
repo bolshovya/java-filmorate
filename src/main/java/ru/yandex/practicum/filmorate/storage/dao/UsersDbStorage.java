c52848bdd65efbcc1512ec676d851f3271fb994b
bolshovya <bolshov.yv@yandex.ru>
2023-06-23 03:01:42 +0300

    feat: 146 tests passed

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UsersDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UsersDbStorage.java
@@ -82,6 +82,7 @@ public class UsersDbStorage implements UsersStorage {
     public List<User> getFriendListById(int id) {
         String sqlQuery = "SELECT * FROM users WHERE id IN (SELECT friend_id FROM friendships WHERE user_id=?)";
         return jdbcTemplate.query(sqlQuery, new UserMapper(), id);
+
     }
 
     @Override
