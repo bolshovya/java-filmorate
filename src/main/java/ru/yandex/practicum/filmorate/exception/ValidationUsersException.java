ebe411e55b7e1f2c5c6dfa183cb1ce5e4e64ee53
bolshovya <bolshov.yv@yandex.ru>
2023-06-27 01:11:09 +0300

    325 tests passed

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationUsersException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationUsersException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class ValidationUsersException extends RuntimeException {
 
     public ValidationUsersException(String message) {
